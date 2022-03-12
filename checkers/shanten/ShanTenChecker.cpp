84ad29c51f8f5fe573c73a4c55ad7dfc0a8d1c66
ubuntu <ubuntu@localhost.localdomain>
2022-03-12 14:46:59 +0800

    feat(training): Add debug model

---
+++ checkers/shanten/ShanTenChecker.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<shanten.h>
+using namespace std;
+
+void ShantenChecker::CheckValid() {
+
+}
+
+void ShantenChecker::checkSevenPairs(int &sevenPairs) {
+}
+
+void ShantenChecker::CheckSpecialWin(int &thirteenOrpans, int &sevenPairs) {
+
+}
+
+int ShantenChecker::CheckShanTenNum() {
+
+}
